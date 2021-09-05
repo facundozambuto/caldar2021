7075964c7f95caf6cb4ba119c084995a23ed0b40
facundo-zambuto-newshore <37534822+facundo-zambuto-newshore@users.noreply.github.com>
2021-09-05 11:45:16 -0300

    Remove morgan

--- api/app.js
+++ api/app.js
@@ -3,7 +3,7 @@ const app = express();
 const helmet = require('helmet');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
+//const morgan = require('morgan');
 
 // Body Parsers
 app.use(bodyParser.urlencoded({extended: true}));
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 // Others
 app.use(cors());
 app.use(helmet());
-app.use(morgan());
+//app.use(morgan());
 
 // Routes
 app.use(require('./routes/auth.caldar'));
